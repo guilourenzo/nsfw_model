2474f849dca36433874f6405736045e4d609ba1e
Guilherme Lourenco <gui.lourenzo@gmail.com>
2020-10-05 02:40:05 -0300

    .

--- tf1/training/inceptionv3_transfer/train_fine_tune.py
+++ tf1/training/inceptionv3_transfer/train_fine_tune.py
@@ -4,7 +4,9 @@ from keras.backend import clear_session
 from keras.optimizers import SGD
 from pathlib import Path
 from keras.models import Sequential, Model, load_model
+from datetime import datetime
 
+print('Start: ', datetime.now())
 # reusable stuff
 import constants
 import callbacks
@@ -18,7 +20,7 @@ height = constants.SIZES['basic']
 width = height
 weights_file = "weights.best_inception" + str(height) + ".hdf5"
 
-print ('Starting from last full model run')
+print ('Starting from last full model run : ', datetime.now())
 model = load_model("nsfw." + str(width) + "x" + str(height) + ".h5")
 
 # Unlock a few layers deep in Inception v3
@@ -33,7 +35,7 @@ for layer in model.layers:
         layer.trainable = False
 
 # Let's see it
-print('Summary')
+print('Summary : ', datetime.now())
 print(model.summary())
 
 # Load checkpoint if one is found
@@ -44,7 +46,7 @@ if os.path.exists(weights_file):
 # Get all model callbacks
 callbacks_list = callbacks.make_callbacks(weights_file)
 
-print('Compile model')
+print('Compile model: ', datetime.now())
 opt = SGD(momentum=.9)
 model.compile(
     loss='categorical_crossentropy',
@@ -55,7 +57,7 @@ model.compile(
 # Get training/validation data via generators
 train_generator, validation_generator = generators.create_generators(height, width)
 
-print('Start training!')
+print('Start training! : ', datetime.now())
 history = model.fit_generator(
     train_generator,
     callbacks=callbacks_list,
@@ -69,5 +71,33 @@ history = model.fit_generator(
 )
 
 # Save it for later
-print('Saving Model')
-model.save("nsfw." + str(width) + "x" + str(height) + ".h5")
+print('Saving Model: ', datetime.now())
+model.save("new_nsfw." + str(width) + "x" + str(height) + ".h5")
+model.save("nsfw")
+
+print('Finish: ', datetime.now())
+
+final_loss, final_accuracy = model.evaluate(validation_generator, steps = constants.VALIDATION_STEPS)
+
+print("Final loss: {:.2f}".format(final_loss))
+print("Final accuracy: {:.2f}%".format(final_accuracy * 100))
+
+
+import matplotlib as plt 
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
